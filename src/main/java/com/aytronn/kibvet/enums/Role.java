732fa46a6a527df0e586dfa756fa070e06ab655c
zine-eddine95 <zineeddine.chettar@gmail.com>
2023-10-24 09:56:15 +0200

    test

--- src/main/java/com/aytronn/kibvet/enums/Role.java
+++ src/main/java/com/aytronn/kibvet/enums/Role.java
@@ -1,22 +1,48 @@
 package com.aytronn.kibvet.enums;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public enum Role {
-    ADMIN,
-    EDITOR,
-    USER;
+    ADMIN(
+            Set.of(
+                    Permission.DELETE,
+                    Permission.POST,
+                    Permission.PUT,
+                    Permission.GET
+            )
+    ),
+    EDITOR(
+            Set.of(
+                    Permission.POST,
+                    Permission.PUT,
+                    Permission.GET
+            )
+    ),
+    USER(
+            Set.of(
+                    Permission.GET
+            )
+    );
 
+    private final Set<Permission> permissions;
 
-
-    Role() {
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
     }
 
-    /*
     public List<SimpleGrantedAuthority> getAuthorities() {
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        this.permissions.forEach(permission -> {
+            authorities.add(new SimpleGrantedAuthority(permission.getPermission()));
+        });
 
         authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
 
         return authorities;
     }
-     */
 }
