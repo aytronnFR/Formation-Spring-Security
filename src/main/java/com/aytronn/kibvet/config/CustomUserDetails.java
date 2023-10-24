c1505c0a46d072e941ab4a487b4655c36f9beb99
vlaar93 <priamdavid@gmail.com>
2023-10-24 10:23:17 +0200

    Users via localUser

---
+++ src/main/java/com/aytronn/kibvet/config/CustomUserDetails.java
@@ -0,0 +1,56 @@
+package main.java.com.aytronn.kibvet.config;
+
+import com.aytronn.kibvet.dao.LocalUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final LocalUser user;
+
+    public CustomUserDetails(LocalUser user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .collect(Collectors.toList());
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
--- src/main/java/com/aytronn/kibvet/config/SecurityConfig.java
+++ src/main/java/com/aytronn/kibvet/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package main.java.com.aytronn.kibvet.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,40 +9,40 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     // Configuration de l'authentification
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        // Définir les utilisateurs et rôles
-        auth.inMemoryAuthentication()
-                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
-                .and()
-                .withUser("editor").password(passwordEncoder().encode("password")).roles("EDITOR")
-                .and()
-                .withUser("admin").password(passwordEncoder().encode("password")).roles("ADMIN");
-                
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     // Configuration de l'autorisation
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/v1/user").hasAnyRole("USER", "EDITOR")
-                .antMatchers(HttpMethod.POST, "/api/v1/user").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE, "/api/v1/user").hasRole("EDITOR")
-                .antMatchers(HttpMethod.PUT, "/api/v1/user").hasRole("EDITOR")
-                .and()
-                .httpBasic()
-                .and()
-                .csrf().disable();
+            .antMatchers("/api/v1/user").hasAnyRole("USER", "EDITOR")
+            .antMatchers(HttpMethod.POST, "/api/v1/user").hasRole("USER")
+            .antMatchers(HttpMethod.DELETE, "/api/v1/user").hasRole("EDITOR")
+            .antMatchers(HttpMethod.PUT, "/api/v1/user").hasRole("EDITOR")
+            .and()
+            .httpBasic()
+            .and()
+            .csrf().disable();
     }
 
-    // Encoder de mot de passe
+    // Encoder le mot de passe
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
