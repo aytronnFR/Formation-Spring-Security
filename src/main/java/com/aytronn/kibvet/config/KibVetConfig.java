e873a5f81f1ea5a8425244917318bab4e56791da
donyaSo <116372643+donyaSo@users.noreply.github.com>
2023-10-24 10:21:33 +0200

    tp termine

--- src/main/java/com/aytronn/kibvet/config/KibVetConfig.java
+++ src/main/java/com/aytronn/kibvet/config/KibVetConfig.java
@@ -1,7 +1,21 @@
 package com.aytronn.kibvet.config;
 
+import com.aytronn.kibvet.dao.LocalUser;
 import com.aytronn.kibvet.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @Configuration
 public class KibVetConfig {
@@ -10,5 +24,38 @@ public class KibVetConfig {
 
     public KibVetConfig(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return this::loadUserByUsername;
+    }
+
+    public UserDetails loadUserByUsername(String username) {
+        Optional<LocalUser> byUsername = userRepository.findByUsername(username);
+        if (byUsername.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return byUsername.get();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
---
+++ src/main/java/com/aytronn/kibvet/dto/UpdateRoleDto.java
@@ -0,0 +1,8 @@
+package com.aytronn.kibvet.dto;
+
+import com.aytronn.kibvet.enums.Role;
+
+public record UpdateRoleDto (Role role){
+    
+    
+}
